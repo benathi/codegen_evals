c5c5dae8a1b06c2c58d5768d6eb8b99cffc1b95f
Ben Athiwaratkun <ben.athiwaratkun@gmail.com>
2023-03-19 23:16:48 -0400

    adding log

---
+++ README.md
@@ -0,0 +1,39 @@
+## Code Generation Evaluation
+This repository contains a script for evaluating the code generation abilities of GPT models using APIs. The script takes several arguments that allow you to customize the evaluation for your needs. It also allows for multi-turn code evaluation where the model can see the execution error message.
+
+### Usage
+To use the script, follow these steps:
+
+* Clone the repository to your local machine.
+
+* Install the required dependencies by running `pip install -r requirements.txt`.
+
+* Open a terminal and navigate to the repository directory.
+
+* Run the script followed by the desired arguments. For example, to evaluate the gpt-3.5-turbo model on the multi-humaneval dataset with num_shots=1 and num_turns=1, enter the following command:
+
+
+```
+python src/eval_gpt_mxeval.py --model_name gpt-3.5-turbo --dataset multi-humaneval --num_shots 1 --num_turns 1
+```
+
+### Arguments
+The script takes the following arguments:
+
+* `--model_name`: The name of the GPT model to evaluate. Defaults to gpt-3.5-turbo. Options are gpt-3.5-turbo or gpt-4.
+* `--dataset`: The dataset to evaluate the model on. Defaults to multi-humaneval. Options are multi-humaneval, mbxp, or mathqa-x.
+* `--num_shots`: The number of examples to evaluate the model on. Defaults to 1.
+* `--num_turns`: The number of turns per example. Defaults to 1.
+* `--limit_num_problems`: If not None, the evaluation will be performed on a subset of problems for debugging purposes. Defaults to None.
+* `--verbose`: The verbosity level of the output. Defaults to 0.
+* `--language`: The programming language to evaluate the model on. Defaults to all.
+* `--temp`: The sampling temperature for the GPT model. Defaults to 0.2.
+
+
+### API Configuration
+To use the script with APIs, you'll need to configure the API credentials in the script. By default, the API key is located here.
+
+```
+## YOUR API KEY HERE
+openai.api_key_path = ".openai_key"
+```
--- src/eval_gpt_mxeval.py
+++ src/eval_gpt_mxeval.py
@@ -1,3 +1,4 @@
+import logging
 import openai
 from mxeval.evaluation import (
   get_execute_function,
@@ -9,8 +10,12 @@ from multiprocessing.pool import ThreadPool
 import numpy as np
 import time
 import argparse
+import sys
+import datetime
+
 ## YOUR API KEY HERE
 openai.api_key_path = ".openai_key"
+
 
 
 def get_args():
@@ -29,10 +34,11 @@ def get_args():
   arg_parser.add_argument("--verbose", type=int, default=0,
                           )
   arg_parser.add_argument("--language", type=str, default="all")
+  arg_parser.add_argument("--language_exclude", type=str, default=None)
   arg_parser.add_argument("--temp", type=float, default=0.2)
+  arg_parser.add_argument("--experiment_name", type=str, default=None)
   args = arg_parser.parse_args()
   return args
-
 
 
 def construct_messages(turn_idx,
@@ -77,27 +83,27 @@ def query_code_completion(model_name,
                           kwargs={},
                           retry_count=5):
   # calling API
-  print("Initiating the API call")
+  logging.info("Initiating the API call")
   request_result = None
   for i in range(retry_count):
-    print(f"API Call number {i + 1}")
+    logging.info(f"API Call number {i + 1}")
     try:
       request_result = openai.ChatCompletion.create(
         model=model_name,
         messages=messages,
-        timeout=10,
+        timeout=1.0,
         **kwargs,
       )
       break
     except Exception as e:
-      print(e)
-      time.sleep(2)
+      logging.warning(e)
+      time.sleep(1)
   if request_result is None:
     response = ""
-    print(f"Warning!! -- API call failed after {retry_count} retries")
+    logging.warning(f"Warning!! -- API call failed after {retry_count} retries")
   else:
     if verbose:
-      print(request_result)
+      logging.debug(request_result)
     response = request_result["choices"][0]["message"]["content"]
 
   return response, messages, request_result #code, request_result
@@ -167,13 +173,14 @@ def eval_language(dataset="mbxp",
         )
 
         code = response
-        print("\n\n\n\n\n\n")
-        print(f"code from query (answer full function ={answer_full_function})")
-        print("################################################################################")
-        print(prompt)
-        print("-----------------[ completion ]------------------")
-        print(code)
-        print("################################################################################")
+        s = "\n\n\n\n\n\n"
+        s += f"code from query (answer full function ={answer_full_function})\n"
+        s += ("################################################################################\n")
+        s += (prompt)
+        s += ("\n-----------------[ completion ]------------------\n")
+        s += (code)
+        s += ("\n################################################################################\n")
+        logging.info(s)
         kwargs = {"problem": problem,
                   "completion": code,
                   "timeout": 30.0,
@@ -184,8 +191,8 @@ def eval_language(dataset="mbxp",
             future = executor.submit(check_correctness_function, **kwargs)
             result = future.result()
             if verbose:
-              print(f"Task idx = {task_id} | rep {rep} | passed {result['passed']}")
-              print("################################################################################")
+              logging.info(f"Task idx = {task_id} | rep {rep} | passed {result['passed']}")
+              logging.info("################################################################################")
             passed = result["passed"]
 
             if passed:
@@ -194,9 +201,10 @@ def eval_language(dataset="mbxp",
               previous_response = code
               execution_result = result["result"]
               if verbose > 2:
-                print("*************** execution result ****************")
-                print(execution_result)
-                print("**************************************************")
+                s = ("*************** execution result ****************\n")
+                s += (execution_result)
+                s += ("\n**************************************************\n")
+                logging.info(s)
       passes.append(passed)
     if execute:
       assert len(passes) == num_reps, f"Number of passes does not match number of attempts -- {len(passes)} != {num_reps} -- {passes}"
@@ -211,13 +219,45 @@ def eval_language(dataset="mbxp",
       total_list.append(total[task_id])
       correct_list.append(correct[task_id])
     pass_at_k = wrapper_pass_at_k(k_list=[1, num_reps], total=total_list, correct=correct_list)
-    print(pass_at_k)
+    logging.info(pass_at_k)
 
+
+
+
+def get_log_filename(args, experiment_name=None):
+    # Generate experiment name based on args
+    experiment_name = experiment_name or "experiment"
+    exclude = "_minus" + args.language_exclude if args.language_exclude else ""
+    experiment_name += f"_{args.model_name}_{args.dataset}_{args.language}{exclude}_shots{args.num_shots}_turns{args.num_turns}"
+    if args.limit_num_problems:
+        experiment_name += f"_limit{args.limit_num_problems}"
+    if args.temp != 0.2:
+        experiment_name += f"_temp{args.temp}"
+    if args.verbose > 0:
+        experiment_name += f"_verbose{args.verbose}"
+    if args.experiment_name:
+        experiment_name += f"_{args.experiment_name}"
+
+    # Generate filename with timestamp
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"logs/{experiment_name}_{now}.log"
+    return filename
 
 def eval_all_langs():
   args = get_args()
+
+  # Set up logging
+  logging.basicConfig(level=logging.DEBUG,
+                      format='%(asctime)s %(levelname)s %(message)s',
+                      datefmt='%Y-%m-%d %H:%M:%S')
+  fh = logging.FileHandler(get_log_filename(args))
+  fh.setLevel(logging.DEBUG)
+  fh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+  logging.getLogger().addHandler(fh)
+
   for language in get_supported_langs(args.dataset):
-    if args.language != "all" and language not in args.language:
+    if (args.language != "all" and language not in args.language) or \
+      (args.language_exclude is not None and language in args.language_exclude):
       continue
     eval_language(dataset=args.dataset,
          model_name=args.model_name,
@@ -228,6 +268,7 @@ def eval_all_langs():
          temperature=args.temp,
          k_shot=args.num_shots,
          )
+  logging.shutdown()
 
 if __name__ == "__main__":
   eval_all_langs()
